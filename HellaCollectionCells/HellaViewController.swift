61e981cdd7d6690fad35907f9df8f723c2e9863e
edmund-h <447ght@gmail.com>
2017-02-15 17:22:16 -0500

    ran into a really weird swift bug why

--- HellaCollectionCells/HellaViewController.swift
+++ HellaCollectionCells/HellaViewController.swift
@@ -8,15 +8,60 @@
 
 import UIKit
 
-class HellaViewController: UIViewController {
-
+class HellaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var fibby = Set<Int>()
+    
+    @IBOutlet weak var fibbyView: UICollectionView!
+    
     override func viewDidLoad() {
-    // Do any additional setup after loading the view, typically from a nib.
+    setUpFibby()
+    fibbyView.delegate = self
+    fibbyView.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "fibbyCell", for: indexPath)
+        //let oldColor = cell.backgroundColor
+        if fibby.contains(where: {$0 == indexPath.row} ){
+            cell.backgroundColor = UIColor.purple
+        } else {cell.backgroundColor = UIColor.yellow}
+        return cell
+    }
+    
+    func setUpFibby (){
+        
+        fibby.insert(0)
+        var oldFib = 1
+        fibby.insert(oldFib)
+        var newFib = 1
+        while oldFib < 100{
+            let hipsterFib = newFib + oldFib
+            print ("inserting \(hipsterFib)")
+            fibby.insert (hipsterFib)
+            oldFib = newFib
+            newFib = hipsterFib
+        }
+        
+    }
+}
+
+ {
+    init {
+        self.red
     }
 }
 
